a859cc8be5ecfd240d92b40f3d7ac9ee49da87aa
Stepchik1 <157306287+Stepchik1@users.noreply.github.com>
2024-09-21 13:28:53 +0700

    Add files via upload

---
+++ blur/Pi1.js
@@ -0,0 +1,94 @@
+
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text1');
+    textElement.classList.toggle('blurred');
+});
+    
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text3');
+    textElement.classList.toggle('blurred');
+});
+
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text6');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text7');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text8');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text9');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text10');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text11');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text12');
+    textElement.classList.toggle('blurred');
+});
+
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text13');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text14');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text15');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text16');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text17');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text18');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text19');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text20');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton1').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text21');
+    textElement.classList.toggle('blurred');
+});
---
+++ blur/R1.js
@@ -0,0 +1,86 @@
+
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text1');
+    textElement.classList.toggle('blurred');
+});
+    
+
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text4');
+    textElement.classList.toggle('blurred');
+});
+
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text7');
+    textElement.classList.toggle('blurred');
+});
+
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text9');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text10');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text11');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text12');
+    textElement.classList.toggle('blurred');
+});
+
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text13');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text14');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text15');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text16');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text17');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text18');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text19');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text20');
+    textElement.classList.toggle('blurred');
+});
+document.getElementById('blurButton2').addEventListener('click', 
+    function() {
+    const textElement = document.getElementById('text21');
+    textElement.classList.toggle('blurred');
+});
